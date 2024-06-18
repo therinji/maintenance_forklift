51c10c23cabb234057dbb092e895745dcd077d76
therinji <rindho2704@gmail.com>
2024-06-18 21:23:35 +0700

    update db

---
+++ maintenance_forklift.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Jun 2024 pada 16.22
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maintenance_forklift`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `forklift`
+--
+-- Kesalahan membaca struktur untuk tabel maintenance_forklift.forklift: #1932 - Table 'maintenance_forklift.forklift' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel maintenance_forklift.forklift: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `maintenance_forklift`.`forklift`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `maintenance`
+--
+-- Kesalahan membaca struktur untuk tabel maintenance_forklift.maintenance: #1932 - Table 'maintenance_forklift.maintenance' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel maintenance_forklift.maintenance: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `maintenance_forklift`.`maintenance`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `operator`
+--
+-- Kesalahan membaca struktur untuk tabel maintenance_forklift.operator: #1932 - Table 'maintenance_forklift.operator' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel maintenance_forklift.operator: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `maintenance_forklift`.`operator`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengajuan`
+--
+-- Kesalahan membaca struktur untuk tabel maintenance_forklift.pengajuan: #1932 - Table 'maintenance_forklift.pengajuan' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel maintenance_forklift.pengajuan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `maintenance_forklift`.`pengajuan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+-- Kesalahan membaca struktur untuk tabel maintenance_forklift.petugas: #1932 - Table 'maintenance_forklift.petugas' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel maintenance_forklift.petugas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `maintenance_forklift`.`petugas`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
